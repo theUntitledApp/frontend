a428f7fe65cf73f77c594fbc6b2ed57546240875
Adnan <artoox61@googlemail.com>
2023-03-10 18:51:56 +0100

    feat(Picker): refactor

--- src/components/Picker/Picker.tsx
+++ src/components/Picker/Picker.tsx
@@ -1,29 +1,19 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent } from "react";
 import { Dimensions, StyleSheet, Text, View } from "react-native";
-import { PanGestureHandler } from "react-native-gesture-handler";
 import Animated, {
-  Easing,
   Extrapolate,
   interpolate,
-  useAnimatedGestureHandler,
-  useAnimatedReaction,
   useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
-  withTiming,
 } from "react-native-reanimated";
 
 import colors from '../colors';
-
+import PickerGestureHandler from "./PickerGestureHandler";
 import { ITEM_HEIGHT, VISIBLE_ITEMS, DURATION } from "./PickerConstants";
-import { GestureProps, PickerProps } from './PickerTypes';
+import { PickerProps } from './PickerTypes';
 
 const { width: WIDTH } = Dimensions.get("window");
-
-const timingConfig = {
-  duration: DURATION,
-  easing: Easing.bezier(0.22, 1, 0.36, 1),
-}
 
 const styles = StyleSheet.create({
   appContainer: {
@@ -56,67 +46,6 @@ const styles = StyleSheet.create({
   },
 
 })
-
-// finds closes point to the wanted SnapPoint
-const findClosestSnapPoint = (snapPointValue: number, velocity: number, points: number[]) => {
-  'worklet';
-  let maxValue: number = Infinity;
-  const snapPoint = snapPointValue + 0.2 * velocity;
-  let closestsSnapPoint = snapPoint;
-
-  for (let i = 0; i < points.length; i++) {
-    const point = points[i];
-    const distance = Math.abs(snapPoint - point);
-    const distanceSmallerCurrentMax = maxValue > distance;
-    maxValue = distanceSmallerCurrentMax ? distance : maxValue;
-    closestsSnapPoint = distanceSmallerCurrentMax ? point : closestsSnapPoint;
-  }
-
-  return closestsSnapPoint;
-}
-
-const usePanGestureHandler = (snapPoints: number[]) => {
-  const offset = useSharedValue(-ITEM_HEIGHT);
-  const position = useSharedValue(offset.value);
-  const toValue = useSharedValue(0);
-
-  const gestureHandler = useAnimatedGestureHandler({
-    onStart: () => {
-      offset.value = position.value;
-    },
-    onActive: (event: { translationY: number, velocityY: number }) => {
-      position.value = offset.value + event.translationY;
-      toValue.value = findClosestSnapPoint(position.value, event.velocityY, snapPoints);
-    },
-    onEnd: () => {
-      position.value = withTiming(toValue.value, timingConfig);
-    }
-  });
-
-  return { position, gestureHandler };
-};
-
-
-const GestureHandler: FunctionComponent<GestureProps> = (props) => {
-  const { maxValue, pickerTranslateY } = props;
-  const snapPoints = new Array(maxValue).fill(0).map((_, i) => i * -ITEM_HEIGHT);
-  const { position, gestureHandler } = usePanGestureHandler(snapPoints);
-
-  useAnimatedReaction(
-    () => {
-      position.value;
-    },
-    () => {
-      pickerTranslateY.value = position.value + ITEM_HEIGHT * 2;
-    },
-    []
-  );
-  return (
-    <PanGestureHandler onGestureEvent={gestureHandler}>
-      <Animated.View style={[StyleSheet.absoluteFillObject, { backgroundColor: "rgba(255, 255, 255, 0,1)" }]} />
-    </PanGestureHandler>
-  )
-}
 
 const Picker: FunctionComponent<PickerProps> = (props) => {
   const { values } = props;
@@ -154,7 +83,7 @@ const Picker: FunctionComponent<PickerProps> = (props) => {
           );
         })}
       </Animated.View>
-      <GestureHandler pickerTranslateY={translateY} maxValue={values.length} />
+      <PickerGestureHandler pickerTranslateY={translateY} maxValue={values.length} />
     </View>
   )
 }
---
+++ src/components/Picker/PickerGestureHandler.tsx
@@ -0,0 +1,82 @@
+import React, { FunctionComponent } from "react";
+import { StyleSheet } from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  Easing,
+  useAnimatedGestureHandler,
+  useSharedValue,
+  useAnimatedReaction,
+  withTiming,
+} from "react-native-reanimated";
+
+import { ITEM_HEIGHT, DURATION } from './PickerConstants';
+import { GestureProps } from './PickerTypes';
+
+const timingConfig = {
+  duration: DURATION,
+  easing: Easing.bezier(0.22, 1, 0.36, 1),
+}
+
+// finds closes point to the wanted SnapPoint
+const findClosestSnapPoint = (snapPointValue: number, velocity: number, points: number[]) => {
+  'worklet';
+  let maxValue: number = Infinity;
+  const snapPoint = snapPointValue + 0.2 * velocity;
+  let closestsSnapPoint = snapPoint;
+
+  for (let i = 0; i < points.length; i++) {
+    const point = points[i];
+    const distance = Math.abs(snapPoint - point);
+    const distanceSmallerCurrentMax = maxValue > distance;
+    maxValue = distanceSmallerCurrentMax ? distance : maxValue;
+    closestsSnapPoint = distanceSmallerCurrentMax ? point : closestsSnapPoint;
+  }
+
+  return closestsSnapPoint;
+}
+
+const usePanGestureHandler = (snapPoints: number[]) => {
+  const offset = useSharedValue(-ITEM_HEIGHT);
+  const position = useSharedValue(offset.value);
+  const toValue = useSharedValue(0);
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: () => {
+      offset.value = position.value;
+    },
+    onActive: (event: { translationY: number, velocityY: number }) => {
+      position.value = offset.value + event.translationY;
+      toValue.value = findClosestSnapPoint(position.value, event.velocityY, snapPoints);
+    },
+    onEnd: () => {
+      position.value = withTiming(toValue.value, timingConfig);
+    }
+  });
+
+  return { position, gestureHandler };
+};
+
+
+const PickerGestureHandler: FunctionComponent<GestureProps> = (props) => {
+  const { maxValue, pickerTranslateY } = props;
+  const snapPoints = new Array(maxValue).fill(0).map((_, i) => i * -ITEM_HEIGHT);
+  const { position, gestureHandler } = usePanGestureHandler(snapPoints);
+
+  useAnimatedReaction(
+    () => {
+      position.value;
+    },
+    () => {
+      pickerTranslateY.value = position.value + ITEM_HEIGHT * 2;
+    },
+    []
+  );
+  return (
+    <PanGestureHandler onGestureEvent={gestureHandler}>
+      <Animated.View style={[StyleSheet.absoluteFillObject, { backgroundColor: "rgba(255, 255, 255, 0,1)" }]} />
+    </PanGestureHandler>
+  )
+}
+
+export default PickerGestureHandler;
+
